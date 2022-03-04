758b99e68388aa1ed82ad5a91e11c403eec785ee
EslamSallam <eslam.salam1369@gmail.com>
2022-03-04 18:49:43 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ProblemSolving
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            long x;
+            long y;
+            input = Console.ReadLine();
+            string[] inputs = input.Split(' ');
+            x = long.Parse(inputs[0]);
+            y = long.Parse(inputs[1]);
+            Console.WriteLine(x.ToString() + " + " + y.ToString() + " = " + (x + y).ToString());
+            Console.WriteLine(x.ToString() + " * " + y.ToString() + " = " + (x * y).ToString());
+            Console.WriteLine(x.ToString() + " - " + y.ToString() + " = " + (x - y).ToString());
+        }
+    }
+}
