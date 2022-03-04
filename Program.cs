4101ab3faf383c3d1589e83e78dce2c96f2ae96f
EslamSallam <eslam.salam1369@gmail.com>
2022-03-04 18:50:29 +0200

    C - Simple Calculator

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ namespace ProblemSolving
             long y;
             input = Console.ReadLine();
             string[] inputs = input.Split(' ');
+           
             x = long.Parse(inputs[0]);
             y = long.Parse(inputs[1]);
             Console.WriteLine(x.ToString() + " + " + y.ToString() + " = " + (x + y).ToString());
